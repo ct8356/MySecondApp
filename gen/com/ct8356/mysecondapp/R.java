e1e3159d917d4f906e6ab6200e115b8da33c6d84
Chris Tober <ct8356@my.bristol.ac.uk>
2014-05-19 12:12:14 +0100

    Removed the old MainActivity and DisplayMessageActivity

--- gen/com/ct8356/mysecondapp/R.java
+++ gen/com/ct8356/mysecondapp/R.java
@@ -1091,8 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_search=0x7f050041;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1103,12 +1102,11 @@ containing a value of this type.
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
-        public static final int edit_message=0x7f05003d;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
-        public static final int fragment_time_accumulator_lin=0x7f05003e;
+        public static final int fragment_time_accumulator_lin=0x7f05003d;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1140,7 +1138,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003f;
+        public static final int textView1=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1198,25 +1196,18 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_choose_tag=0x7f030018;
-        public static final int activity_display_message=0x7f030019;
-        public static final int activity_main=0x7f03001a;
-        public static final int activity_start_session=0x7f03001b;
-        public static final int activity_time_accumulator=0x7f03001c;
-        public static final int fragment_choose_tag=0x7f03001d;
-        public static final int fragment_display_message=0x7f03001e;
-        public static final int fragment_main=0x7f03001f;
-        public static final int fragment_start_session=0x7f030020;
-        public static final int fragment_time_accumulator=0x7f030021;
-        public static final int notes_row=0x7f030022;
-        public static final int support_simple_spinner_dropdown_item=0x7f030023;
+        public static final int activity_start_session=0x7f030019;
+        public static final int activity_time_accumulator=0x7f03001a;
+        public static final int fragment_choose_tag=0x7f03001b;
+        public static final int fragment_start_session=0x7f03001c;
+        public static final int fragment_time_accumulator=0x7f03001d;
+        public static final int notes_row=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
         public static final int choose_tag=0x7f0c0000;
-        public static final int display_message=0x7f0c0001;
-        public static final int main=0x7f0c0002;
-        public static final int main_activity_actions=0x7f0c0003;
-        public static final int start_session=0x7f0c0004;
-        public static final int time_accumulator=0x7f0c0005;
+        public static final int start_session=0x7f0c0001;
+        public static final int time_accumulator=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
